1c39d10caea3d35de4fda1386016c3c9ab79b9b3
Vincent Wilms <git@m1.apollo3zehn.net>
2025-04-10 16:02:48 +0200

    Done

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,24 @@
+mod basket;
+mod stack;
+mod container;
+
+use std::vec;
+
+use basket::Basket;
+use container::Container;
+use stack::Stack;
+
+fn add_string<T: Container<String>>(container: &mut T, s: String) {
+    container.put(s);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut b1 = Basket::new(String::from("hi there"));
+    let b2 = Basket::new(10);
+    let b3 = Basket::new(true);
+
+    let mut s1 = Stack::new(vec![String::from("hi")]);
+    let s2 = Stack::new(vec![1, 2, 3]);
+
+    add_string(&mut s1, String::from("hi"));
 }
